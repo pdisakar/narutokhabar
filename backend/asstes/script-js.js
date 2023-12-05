59f05f6f2d732e40a1fa8b29db1eb5637f1ce4c2
Sakar Pudasaini <pdi.sakar@gmail.com>
2023-12-05 12:50:13 +0545

    Minor Fix fourth attempt

---
+++ backend/asstes/script-js.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", (event) => {
+  document
+    .getElementById("avatarImage")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      document.getElementById("avatarImageUpload").click();
+    });
+
+  document
+    .getElementById("avatarImageUpload")
+    .addEventListener("change", function (e) {
+      e.preventDefault();
+      var fileInput = this;
+      var avatarImage = document.getElementById("avatarImage");
+
+      if (fileInput.files && fileInput.files[0]) {
+        if (!fileInput.files[0].type.startsWith("image/")) {
+          alert("Please upload a valid image file.");
+          fileInput.value = "";
+          return;
+        }
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          e.preventDefault();
+          avatarImage.src = e.target.result;
+        };
+        reader.readAsDataURL(fileInput.files[0]);
+      }
+    });
+
+  document
+    .querySelector(".button-wrap .button:last-child")
+    .addEventListener("click", function () {
+      document.getElementById("name").value = "";
+      document.getElementById("news-title").value = "";
+      document.getElementById("news-discription").value = "";
+
+      document.getElementById("avatarImage").src =
+        "../backend/source/upload-img.png";
+      document.getElementById("avatarImageUpload").value = "";
+
+      document.getElementById("generalImage").src =
+        "../backend/source/default-img.png";
+      document.getElementById("generalImageUpload").value = "";
+    });
+});
