505a6de4dafde21dee54ebf41f29f7b8876d08cd
Sakar Pudasaini <pdi.sakar@gmail.com>
2023-12-05 15:47:39 +0545

    Workiing on validation

--- backend/asstes/script-js.js
+++ backend/asstes/script-js.js
@@ -30,19 +30,54 @@ window.addEventListener("load", (event) => {
       }
     });
 
-  document
-    .querySelector(".discard-btn")
-    .addEventListener("click", function () {
-      document.getElementById("name").value = "";
-      document.getElementById("news-title").value = "";
-      document.getElementById("news-discription").value = "";
+  document.querySelector(".discard-btn").addEventListener("click", function () {
+    document.getElementById("name").value = "";
+    document.getElementById("news-title").value = "";
+    document.getElementById("news-discription").value = "";
 
-      document.getElementById("avatarImage").src =
-        "../backend/source/upload-img.png";
-      document.getElementById("avatarImageUpload").value = "";
+    document.getElementById("avatarImage").src =
+      "../backend/source/upload-img.png";
+    document.getElementById("avatarImageUpload").value = "";
 
-      document.getElementById("generalImage").src =
-        "../backend/source/default-img.png";
-      document.getElementById("generalImageUpload").value = "";
-    });
+    document.getElementById("generalImage").src =
+      "../backend/source/default-img.png";
+    document.getElementById("generalImageUpload").value = "";
+  });
 });
+
+function validateForm() {
+  var avatarImageUpload = document.getElementById("avatarImageUpload");
+  var nameInput = document.getElementById("name");
+  var newsTitleInput = document.getElementById("news-title");
+  var newsDescriptionInput = document.getElementById("news-discription");
+
+  if (
+    !avatarImageUpload.hasAttribute("data-selected") ||
+    avatarImageUpload.getAttribute("data-selected") !== "true"
+  ) {
+    alert("Please choose a new avatar image.");
+    return false;
+  }
+
+  // Validate news title
+  if (nameInput.value.trim() === "") {
+    alert("Please enter a news title.");
+    return false;
+  }
+
+  // Validate news date
+  if (newsTitleInput.value.trim() === "") {
+    alert("Please enter a news date.");
+    return false;
+  }
+
+  // Validate news description
+  if (newsDescriptionInput.value.trim() === "") {
+    alert("Please enter a news description.");
+    return false;
+  }
+
+  // Additional validation logic can be added here if needed
+
+  return true; // Continue with form submission
+}
